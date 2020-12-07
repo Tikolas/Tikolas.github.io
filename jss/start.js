14d275ff7b21c8963d647e395d96bfc574e2b234
Tikolas <40771679+Tikolas@users.noreply.github.com>
2020-12-07 16:21:12 +0300

    005

--- jss/start.js
+++ jss/start.js
@@ -2,7 +2,7 @@ a=5;
 function sec() {
  a--;
   document.getElementById('a').innerHTML=a;
-  if(a==0){document.location.href="head.html";}
+  if(a==0){document.location.href="index.html";}
 }
 function cancel(){a= -1;}
 setInterval(sec, 1000);
